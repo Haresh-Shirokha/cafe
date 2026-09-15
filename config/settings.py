501ff8a30e112f1a004b3053fd48042301bc882d
Hareshkumar shirokha <143522002+Haresh-Shirokha@users.noreply.github.com>
2026-09-15 13:08:11 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -55,6 +55,7 @@ OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY", "")
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
